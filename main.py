5673cd3f92c7266c3a85ac93d6a7d32ca52a3fc3
matt-m-111 <mmarks111@hotmail.com>
2025-07-16 08:41:22 -0400

    screen working

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 import pygame
-
+# import the connect_database function
+# and the database_version variable
+# from database.py into the current file
+from constants import *
 
 
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
     main()
+
